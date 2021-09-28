39070fd1a704a2c8bcf7794c7c9d8060bbc49a86
James Harrison <jamesebharrison@hotmail.com>
2021-09-28 14:30:59 +0100

    added daysUntilTheWeekend in onblur

--- script.js
+++ script.js
@@ -12,22 +12,22 @@ switch(days){
     case "Monday":
         days = "Five more days left"
         break;
-        case "Tuesday":
+    case "Tuesday":
         days = "Four more days left"
         break;
-        case "Wednesday":
+    case "Wednesday":
         days = "Three more days left"
         break;
-        case "Thursday":
+    case "Thursday":
         days = "Two more days left"
         break;
-        case "Friday":
+    case "Friday":
         days = "One more day left put on your party shoes!"
         break;
-        case "Saturday":
+    case "Saturday":
         days = "It's the weekend baby let's groove tonight"
         break;
-        case "Sunday":
+    case "Sunday":
         days = "Well that was quick, let's take it easy today"
         break;
 
