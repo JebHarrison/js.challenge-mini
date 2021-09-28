52e6963f103a57fccacc94a54b8255ba960168e6
James Harrison <jamesebharrison@hotmail.com>
2021-09-28 14:06:56 +0100

    added a switch case

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ const myNumber = 3;
 } else {
     console.log("It's a negative number")
 }
+
+
+switch(days){
+    case "Monday":
+        days = "Five more days left"
+        break;
+        case "Tuesday":
+        days = "Four more days left"
+        break;
+        case "Wednesday":
+        days = "Three more days left"
+        break;
+        case "Thursday":
+        days = "Two more days left"
+        break;
+        case "Friday":
+        days = "One more day left put on your party shoes!"
+        break;
+        case "Saturday":
+        days = "It's the weekend baby let's groove tonight"
+        break;
+        case "Sunday":
+        days = "Well that was quick, let's take it easy today"
+        break;
+}
